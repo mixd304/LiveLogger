ad03bfe7abccc2ca6ed0285089705189591ff495
Darius Mix <d.mix@dvz-mv.de>
2019-06-27 08:01:50 +0200

    Logdatei als Textdatei speichern

--- src/main/java/ViewController/SecondPaneController/LogFilePage_Controller.java
+++ src/main/java/ViewController/SecondPaneController/LogFilePage_Controller.java
@@ -13,7 +13,13 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.GridPane;
 
+import javax.swing.*;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class LogFilePage_Controller {
     @FXML Label log_1_label;
@@ -25,12 +31,14 @@ public class LogFilePage_Controller {
     @FXML ListView<String> log_3;
     @FXML ListView<String> log_4;
     @FXML GridPane logFilePage;
+    ArrayList<Label> labelList;
+    ArrayList<ListView<String>> listviewList;
 
     @FXML
     private void initialize() {
         System.out.println("[INIT] logFilePage geladen");
-        ArrayList<Label> labelList = new ArrayList<>();
-        ArrayList<ListView<String>> listviewList = new ArrayList<>();
+        labelList = new ArrayList<>();
+        listviewList = new ArrayList<>();
         labelList.add(log_1_label);
         labelList.add(log_2_label);
         labelList.add(log_3_label);
@@ -77,11 +85,58 @@ public class LogFilePage_Controller {
             }
         });
 
+        // MenuItem - Als Textdatei Speichern
+        MenuItem menuItem_safeAsTxt = new MenuItem("Als Textdatei speichern");
+        menuItem_safeAsTxt.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("[ACTION] Zeile - Als Textdatei speichern geklickt");
+                contextMenu.hide();
+
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setDialogTitle("Speicherort auswählen");
+                fileChooser.setFileHidingEnabled(false);
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+                int returnVal = fileChooser.showOpenDialog(null);
+                String path = null;
+                if(returnVal == JFileChooser.APPROVE_OPTION) {
+                    path = fileChooser.getSelectedFile().getAbsolutePath();
+                }
+
+                if(path == null) {
+                    return;
+                }
+
+                for (Label l: labelList) {
+                    if(l.getId().equals(listview.getId() + "_label")) {
+                        DateFormat dateFormat = new SimpleDateFormat("yyyy MM dd HH.mm-ss");
+                        Date date = new Date();
+                        path = path + "/[" + dateFormat.format(date) + "] " + l.getText().trim() + ".txt";
+                    }
+                }
+                File file = new File(path);
+
+                System.out.println("[INFO]   Speichere Logausgaben in Textdatei " + file);
+                try {
+                    PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(file, true)), true);
+                    for(String s: (ObservableList<String>) listview.getItems()) {
+                        printWriter.println(s);
+                        printWriter.flush();
+                    }
+                    printWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         // TODO: Kontextmenü erweitern - ListView
 
         // Alle MenuItems zum ContextMenu hinzufügen
         contextMenu.getItems().add(menuItem_open);
         contextMenu.getItems().add(menuItem_copy);
+        contextMenu.getItems().add(menuItem_safeAsTxt);
         listview.setContextMenu(contextMenu);
     }
 }
